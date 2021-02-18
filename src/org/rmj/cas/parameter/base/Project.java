a61339eaa8fc9e965ed7bd130e9e252e7223107a
Michael Cuison <xurpas7@gmail.com>
2021-02-18 10:02:32 +0800

    engr-parameter-base init

---
+++ src/org/rmj/cas/parameter/base/Project.java
@@ -0,0 +1,300 @@
+/**
+ * @author  Michael Cuison
+ * @date    2020-01-19
+ */
+package org.rmj.cas.parameter.base;
+
+import com.mysql.jdbc.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.rmj.appdriver.GRider;
+import org.rmj.appdriver.MiscUtil;
+import org.rmj.appdriver.SQLUtil;
+import org.rmj.appdriver.constants.RecordStatus;
+import org.rmj.appdriver.iface.GEntity;
+import org.rmj.appdriver.iface.GRecord;
+import org.rmj.cas.parameter.pojo.UnitProject;
+
+public class Project implements GRecord{   
+    @Override
+    public UnitProject newRecord() {
+        UnitProject loObject = new UnitProject();
+        
+        Connection loConn = null;
+        loConn = setConnection();       
+        
+        //assign the primary values
+        loObject.setProjectCode(MiscUtil.getNextCode(loObject.getTable(), "sProjCode", true, loConn, ""));
+        
+        return loObject;
+    }
+
+    @Override
+    public UnitProject openRecord(String fstransNox) {
+        UnitProject loObject = new UnitProject();
+        
+        Connection loConn = null;
+        loConn = setConnection();   
+        
+        String lsSQL = MiscUtil.addCondition(getSQ_Master(), "sProjCode = " + SQLUtil.toSQL(fstransNox));
+        ResultSet loRS = poGRider.executeQuery(lsSQL);
+        
+        try {
+            if (!loRS.next()){
+                setMessage("No Record Found");
+            }else{
+                //load each column to the entity
+                for(int lnCol=1; lnCol<=loRS.getMetaData().getColumnCount(); lnCol++){
+                    loObject.setValue(lnCol, loRS.getObject(lnCol));
+                }
+            }              
+        } catch (SQLException ex) {
+            setErrMsg(ex.getMessage());
+        } finally{
+            MiscUtil.close(loRS);
+            if (!pbWithParent) MiscUtil.close(loConn);
+        }
+        
+        return loObject;
+    }
+
+    @Override
+    public UnitProject saveRecord(Object foEntity, String fsTransNox) {
+        String lsSQL = "";
+        UnitProject loOldEnt = null;
+        UnitProject loNewEnt = null;
+        UnitProject loResult = null;
+        
+        // Check for the value of foEntity
+        if (!(foEntity instanceof UnitProject)) {
+            setErrMsg("Invalid Entity Passed as Parameter");
+            return loResult;
+        }
+        
+        // Typecast the Entity to this object
+        loNewEnt = (UnitProject) foEntity;
+        
+        
+        // Test if entry is ok
+        if (loNewEnt.getProjectName()== null || loNewEnt.getProjectName().isEmpty()){
+            setMessage("Invalid description detected.");
+            return loResult;
+        }
+        
+        loNewEnt.setModifiedBy(psUserIDxx);
+        loNewEnt.setDateModified(poGRider.getServerDate());
+        
+        
+        // Generate the SQL Statement
+        if (fsTransNox.equals("")){
+            Connection loConn = null;
+            loConn = setConnection();   
+            
+            loNewEnt.setProjectCode(MiscUtil.getNextCode(loNewEnt.getTable(), "sProjCode", false, loConn, psBranchCd));
+            
+            if (!pbWithParent) MiscUtil.close(loConn);
+            
+            //Generate the SQL Statement
+            lsSQL = MiscUtil.makeSQL((GEntity) loNewEnt);
+        }else{
+            //Load previous transaction
+            loOldEnt = openRecord(fsTransNox);
+            
+            //Generate the Update Statement
+            lsSQL = MiscUtil.makeSQL((GEntity) loNewEnt, (GEntity) loOldEnt, "sProjCode = " + SQLUtil.toSQL(loNewEnt.getValue(1)));
+        }
+        
+        //No changes have been made
+        if (lsSQL.equals("")){
+            setMessage("Record is not updated");
+            return loResult;
+        }
+        
+        if (!pbWithParent) poGRider.beginTrans();
+        
+        if(poGRider.executeQuery(lsSQL, loNewEnt.getTable(), "", "") == 0){
+            if(!poGRider.getErrMsg().isEmpty())
+                setErrMsg(poGRider.getErrMsg());
+            else
+            setMessage("No record updated");
+        } else loResult = loNewEnt;
+        
+        if (!pbWithParent) {
+            if (!getErrMsg().isEmpty()){
+                poGRider.rollbackTrans();
+            } else poGRider.commitTrans();
+        }        
+        
+        return loResult;
+    }
+
+    @Override
+    public boolean deleteRecord(String fsTransNox) {
+        UnitProject loObject = openRecord(fsTransNox);
+        boolean lbResult = false;
+        
+        if (loObject == null){
+            setMessage("No record found...");
+            return lbResult;
+        }
+        
+        String lsSQL = "DELETE FROM " + loObject.getTable() + 
+                        " WHERE sProjCode = " + SQLUtil.toSQL(fsTransNox);
+        
+        if (!pbWithParent) poGRider.beginTrans();
+        
+        if (poGRider.executeQuery(lsSQL, loObject.getTable(), "", "") == 0){
+            if (!poGRider.getErrMsg().isEmpty()){
+                setErrMsg(poGRider.getErrMsg());
+            } else setErrMsg("No record deleted.");  
+        } else lbResult = true;
+        
+        if (!pbWithParent){
+            if (getErrMsg().isEmpty()){
+                poGRider.commitTrans();
+            } else poGRider.rollbackTrans();
+        }
+        
+        return lbResult;
+    }
+
+    @Override
+    public boolean deactivateRecord(String fsTransNox) {
+        UnitProject loObject = openRecord(fsTransNox);
+        boolean lbResult = false;
+        
+        if (loObject == null){
+            setMessage("No record found...");
+            return lbResult;
+        }
+        
+        if (loObject.getRecordStat().equalsIgnoreCase(RecordStatus.INACTIVE)){
+            setMessage("Current record is inactive...");
+            return lbResult;
+        }
+        
+        String lsSQL = "UPDATE " + loObject.getTable() + 
+                        " SET  cRecdStat = " + SQLUtil.toSQL(RecordStatus.INACTIVE) + 
+                            ", sModified = " + SQLUtil.toSQL(psUserIDxx) +
+                            ", dModified = " + SQLUtil.toSQL(poGRider.getServerDate()) + 
+                        " WHERE sProjCode = " + SQLUtil.toSQL(loObject.getProjectCode());
+        
+        if (!pbWithParent) poGRider.beginTrans();
+        
+        if (poGRider.executeQuery(lsSQL, loObject.getTable(), "", "") == 0){
+            if (!poGRider.getErrMsg().isEmpty()){
+                setErrMsg(poGRider.getErrMsg());
+            } else setErrMsg("No record deleted.");  
+        } else lbResult = true;
+        
+        if (!pbWithParent){
+            if (getErrMsg().isEmpty()){
+                poGRider.commitTrans();
+            } else poGRider.rollbackTrans();
+        }
+        return lbResult;
+    }
+
+    @Override
+    public boolean activateRecord(String fsTransNox) {
+        UnitProject loObject = openRecord(fsTransNox);
+        boolean lbResult = false;
+        
+        if (loObject == null){
+            setMessage("No record found...");
+            return lbResult;
+        }
+        
+        if (loObject.getRecordStat().equalsIgnoreCase(RecordStatus.ACTIVE)){
+            setMessage("Current record is active...");
+            return lbResult;
+        }
+        
+        String lsSQL = "UPDATE " + loObject.getTable() + 
+                        " SET  cRecdStat = " + SQLUtil.toSQL(RecordStatus.ACTIVE) + 
+                            ", sModified = " + SQLUtil.toSQL(psUserIDxx) +
+                            ", dModified = " + SQLUtil.toSQL(poGRider.getServerDate()) + 
+                        " WHERE sProjCode = " + SQLUtil.toSQL(loObject.getProjectCode());
+        
+        if (!pbWithParent) poGRider.beginTrans();
+        
+        if (poGRider.executeQuery(lsSQL, loObject.getTable(), "", "") == 0){
+            if (!poGRider.getErrMsg().isEmpty()){
+                setErrMsg(poGRider.getErrMsg());
+            } else setErrMsg("No record deleted.");  
+        } else lbResult = true;
+        
+        if (!pbWithParent){
+            if (getErrMsg().isEmpty()){
+                poGRider.commitTrans();
+            } else poGRider.rollbackTrans();
+        }
+        return lbResult;
+    }
+
+    @Override
+    public String getMessage() {
+        return psWarnMsg;
+    }
+
+    @Override
+    public void setMessage(String fsMessage) {
+        this.psWarnMsg = fsMessage;
+    }
+
+    @Override
+    public String getErrMsg() {
+        return psErrMsgx;
+    }
+
+    @Override
+    public void setErrMsg(String fsErrMsg) {
+        this.psErrMsgx = fsErrMsg;
+    }
+
+    @Override
+    public void setBranch(String foBranchCD) {
+        this.psBranchCd = foBranchCD;
+    }
+
+    @Override
+    public void setWithParent(boolean fbWithParent) {
+        this.pbWithParent = fbWithParent;
+    }
+
+    @Override
+    public String getSQ_Master() {
+        return (MiscUtil.makeSelect(new UnitProject()));
+    }
+    
+    //Added methods
+    public void setGRider(GRider foGRider){
+        this.poGRider = foGRider;
+        this.psUserIDxx = foGRider.getUserID();
+        
+        if (psBranchCd.isEmpty()) psBranchCd = foGRider.getBranchCode();
+    }
+    
+    public void setUserID(String fsUserID){
+        this.psUserIDxx  = fsUserID;
+    }
+    
+    private Connection setConnection(){
+        Connection foConn;
+        
+        if (pbWithParent){
+            foConn = (Connection) poGRider.getConnection();
+            if (foConn == null) foConn = (Connection) poGRider.doConnect();
+        }else foConn = (Connection) poGRider.doConnect();
+        
+        return foConn;
+    }
+    
+    //Member Variables
+    private GRider poGRider = null;
+    private String psUserIDxx = "";
+    private String psBranchCd = "";
+    private String psWarnMsg = "";
+    private String psErrMsgx = "";
+    private boolean pbWithParent = false;
+}
